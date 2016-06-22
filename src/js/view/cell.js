0ec2c14ccd243d52e21966a2bd209690ed36e7e5
Mike Koval <MikeKoval@users.noreply.github.com>
2016-06-22 07:47:52 +0300

    Update cell.js
    add drawing cell on canvas

--- src/js/view/cell.js
+++ src/js/view/cell.js
@@ -3,13 +3,17 @@ class Cell {
 	constructor(model, controller) {
 		this.model = model;
 		this.controller = controller;
-		//this.board = this.model.;
+		
+		this.size = 64;
 	}
 
 	draw() {
 		this.ctx.fillStyle = cell.color ? 'white' : 'black';
 		this.ctx.fillRect(
-			this.model.index
+			this.model.index * this.size,
+			this.model.row.index * this.size,
+			this.size,
+			this.size
 		);
 	}
 }
