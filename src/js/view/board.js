dff5c2f55e34ca21ed4edfadf0abe34a9d199a8d
Mike Koval <MikeKoval@users.noreply.github.com>
2016-06-22 07:58:07 +0300

    Update board.js
    BoardView fixes

--- src/js/view/board.js
+++ src/js/view/board.js
@@ -19,20 +19,21 @@ class BoardView {
     build() {
         for(let i = 0; i < this.model.height; i += 1) {
             for(let j = 0; j < this.model.width; j += 1) {
-                let cell = this.model.rows[i].cells[j];
-                //cells.push(
-                //    Helper.elt('td', {'class': cell.color ? 'white' : 'black'})
-                //);
-                //
-
+                let cellModel = this.model.rows[i].cells[j];
+				
+				let cellView = new CellView(cellModel, null); //TODO rename Cell to CellView
+				
+				cellView.draw();
             }
+			
+			
 
-            rows.push(Helper.elt('tr', {}, cells));
+            //rows.push(Helper.elt('tr', {}, cells));
         }
         
     }
 
-    buildCheckers() {
+    /*buildCheckers() {
         this.checkers = [];
 
         for(let i = 0; i < this.model.checkers.length; i += 1) {
@@ -44,18 +45,18 @@ class BoardView {
 
             this.checkers.push(checkerElement);
         }
-    }
+    }*/
     
     draw() {
-        this.rootElem.appendChild(this.boardWrapper);
+        this.rootElem.appendChild(this.canvas);
     }
     
-    drawCheckers() {
+    /*drawCheckers() {
         for(let i = 0; i < this.checkers.length; i += 1) {
             this.boardWrapper
                 .children[this.model.checkers[i].coords.y]
                 .children[this.model.checkers[i].coords.x]
                 .appendChild(this.checkers[i])
         }
-    }
+    }*/
 }
